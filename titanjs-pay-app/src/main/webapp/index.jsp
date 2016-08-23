c26ab0c7357cc1896a8ae0f488600264a0ca8f0a
wengxitao <xitao.weng@fangcang.com>
2016-08-23 16:13:46 +0800

    -

--- titanjs-pay-app/src/main/webapp/index.jsp
+++ titanjs-pay-app/src/main/webapp/index.jsp
@@ -1,10 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-	 <script type="text/javascript" src="js/rsa/Barrett.js"></script>
-	 <script type="text/javascript" src="js/rsa/BigInt.js"></script>
-	 <script type="text/javascript" src="js/rsa/RSA.js"></script>
-	  <script type="text/javascript" src="js/jquery.js"></script>
 	 <script type="text/javascript" src="js/titanpay.js"></script>
 </head>
 <body>
@@ -12,17 +8,18 @@
 <button value="asdfadfasd" id="payButton" >pay</button>
 </body>
 <script type="text/javascript">
-	$(function(){
 		
-		
+
+	window.onload = function()
+	{
+
 	 var pub = "d8b6e03dd8f9bf45157f0d14aedf9a696665641da90cab5114a22b7f6c711f22429c32c99ab76e3ce74de00145bcd50b9d2e7c60cd97a4979a5d0ce4ead9ba61baca1495758d69cc1f76e69db43f1ef1f9c33cd2edb8c726ed17c297a7b9fa3f18e58aef9d3f33f8431a41cc3c0ca7bc5151d33a8691e6506e0439363aec0063";
 	 var expo = "10001";
-		
-	 initTitanPay('127.0.0.1:8082', expo, pub);
+	
+	 titanPayObj.initTitanPay('127.0.0.1:8082', expo, pub);
 		
 	 //GDP 支付采用固定的账户编码
 	 
-//name n, escrowedDate n, goodsId y, goodsdetail n, goodsname n, userId y, ruserId n, amount y, paytype 
 	  var orderInfo = {
 			name:"",   //打开收银台人员姓名  N
 			escrowedDate:"",//保证期时间  N
@@ -33,6 +30,7 @@
 			ruserId:"",//收款方身份标示 N  ,GDP可以指定接受方的   商家联盟可以指定其FCUSERID
 			amount:"",//订单金额
 			payerType:"",//付款人类型   财务 GDP 等
+			checkOrderUrl:'',//可选
 			notify:'http://www.baidu.com'
 		};
 
@@ -41,13 +39,12 @@
 			inAccountCode:"11111",
 			outAccountCode:"53245234532"
 		}
+		
+		document.getElementById('payButton').onclick =function()
+		{
+			titanPayObj.titanPay(orderInfo, businessInfo);
+		}
 
-		$("#payButton").click(function() {
-
-			titanPay(orderInfo, businessInfo);
-		});
-
-	
-	});
+	}
 </script>
 </html>
--- titanjs-pay-app/src/main/webapp/js/titanpay.js
+++ titanjs-pay-app/src/main/webapp/js/titanpay.js
@@ -20,81 +20,117 @@ var businessInfo
 {
 
 }*/
+var titanPayObj = {};
 
-var config = {
-	intervalTime : 3000
-};
-// var timeIntervalObj = null;
-var titan_rsakey = null;
-/**
- * 初始化公钥及地址
- * 
- * @param address
- *            请求地址：127.0.0.1:8080
- * @param empoent
- *            指数
- * @param module
- *            模数
- */
-function initTitanPay(address, empoent, module) {
-	config.address = address;
-	config.empoent = empoent;
-	config.module = module;
+function initTitanPayObj() {
 
-	setMaxDigits(129);
-	titan_rsakey = new RSAKeyPair(config.empoent, "", config.module);
-	createPayForm(address);
+	var config = {
+		intervalTime : 3000
+	};
+	// var timeIntervalObj = null;
+	var titan_rsakey = null;
+	
+	
+	/**
+	 * 初始化公钥及地址
+	 * 
+	 * @param address
+	 *            请求地址：127.0.0.1:8080
+	 * @param empoent
+	 *            指数
+	 * @param module
+	 *            模数titanJrPayRsaObj
+	 */
+	titanPayObj.initTitanPay = function(address, empoent, module) {
+		config.address = address;
+		config.empoent = empoent;
+		config.module = module;
 
-}
+		
+		this.createPayForm(address);
 
-function createPayForm(address) {
-	$('body')
-			.append(
-					"<form action='http://"
-							+ address
-							+ "/pay/trade/titanPay.action'"
-							+ " method='post' id='titan_pay_form' target='_blank'> "
-							+ "<input type='hidden' name='orderInfo' id='titan_orderInfo'/> <input type='hidden' name='businessInfo' id='titan_businessInfo'/> "
-							+
-
-							" </form>");
-
-}
-
-/**
- * 检查订单信息是否合法
- * 
- * @param orderInfo
- */
-function checkOrder(orderInfo) {
-	return true;
-}
-
-/**
- * 根据订单信息生成签名
- * 
- * @param orderInfo
- */
-function titanEncrypted(str) {
-	return encryptedString(titan_rsakey, encodeURIComponent(str));
-
-}
-
-/**
- * 请求支付
- * 
- * @param orderInfo
- *            订单信息
- * @param businessInfo
- *            业务自定义信息
- * @param resultCallBack
- *            用户自定义回调函数
- */
-function titanPay(orderInfo, businessInfo) {
-
-	if (checkOrder(orderInfo)) {
-		$('#titan_orderInfo').val(titanEncrypted(JSON.stringify(orderInfo)));
-		$('#titan_businessInfo').val(titanEncrypted(JSON.stringify(businessInfo)));
-		$('#titan_pay_form').submit();
 	}
-}
+
+	titanPayObj.createPayForm = function(address) {
+
+		var elscript = document.createElement("script");
+		elscript.src = "http://" + address + "/titanjs-pay-app/js/rsa/RSA.js";
+		elscript.setAttribute("type", "text/javascript");
+		document.body.appendChild(elscript);
+
+		var form = document.createElement("form");
+		form.action = 'http://' + address
+				+ '/titanjs-pay-app/trade/titanPay.action';
+		form.target = '_blank';
+		form.id = 'titan_pay_form';
+		form.method = 'post';
+
+		var orderInfo = document.createElement("input");
+		orderInfo.type = 'hidden';
+		orderInfo.name = 'orderInfo';
+		orderInfo.id = 'titan_orderInfo';
+
+		var businessInfo = document.createElement("input");
+		businessInfo.type = 'hidden';
+		businessInfo.name = 'businessInfo';
+		businessInfo.id = 'titan_businessInfo';
+
+		form.appendChild(orderInfo);
+		form.appendChild(businessInfo);
+		document.body.appendChild(form);
+	}
+
+	/**
+	 * 检查订单信息是否合法
+	 * 
+	 * @param orderInfo
+	 */
+	titanPayObj.checkOrder = function(orderInfo) {
+		return true;
+	}
+
+	/**
+	 * 根据订单信息生成签名
+	 * 
+	 * @param orderInfo
+	 */
+	titanPayObj.titanEncrypted = function(str) {
+		
+
+		if (titan_rsakey == null) {
+			titanJrPayRsaObj.setMaxDigits(129);
+			titan_rsakey = new titanJrPayRsaObj.RSAKeyPair(config.empoent, "",
+					config.module);
+		}
+		
+		return titanJrPayRsaObj.encryptedString(titan_rsakey,
+				encodeURIComponent(str));
+	}
+
+	/**
+	 * 请求支付
+	 * 
+	 * @param orderInfo
+	 *            订单信息
+	 * @param businessInfo
+	 *            业务自定义信息
+	 * @param resultCallBack
+	 *            用户自定义回调函数
+	 */
+	titanPayObj.titanPay = function(orderInfo, businessInfo) {
+
+		if (this.checkOrder(orderInfo)) {
+
+			document.getElementById('titan_orderInfo').value = this
+					.titanEncrypted(JSON.stringify(orderInfo));
+			document.getElementById('titan_businessInfo').value = JSON
+					.stringify(businessInfo);
+			document.getElementById('titan_pay_form').submit();
+			// $('#titan_orderInfo').val();
+			// $('#titan_businessInfo').val(JSON.stringify(businessInfo));
+			// $('#titan_pay_form').submit();
+		}
+	}
+};
+
+initTitanPayObj();
