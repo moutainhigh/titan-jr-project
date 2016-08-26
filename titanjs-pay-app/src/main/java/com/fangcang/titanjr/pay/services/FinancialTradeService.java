1ce1c6bd5a28a1c544cdb4a17e5c19f69026031c
wengxitao <xitao.weng@fangcang.com>
2016-08-26 10:54:12 +0800

    -

--- titanjs-pay-app/src/main/java/com/fangcang/titanjr/pay/services/FinancialTradeService.java
+++ titanjs-pay-app/src/main/java/com/fangcang/titanjr/pay/services/FinancialTradeService.java
@@ -12,15 +12,23 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
+import com.fangcang.titanjr.common.enums.PayerTypeEnum;
 import com.fangcang.titanjr.common.util.HttpUtils;
 import com.fangcang.titanjr.common.util.MD5;
+import com.fangcang.titanjr.dto.request.TitanOrderRequest;
+import com.fangcang.titanjr.facade.TitanFinancialPermissionFacade;
 import com.fangcang.titanjr.pay.req.TitanConfirmBussOrderReq;
 import com.fangcang.titanjr.pay.req.TitanNotifyPayResultReq;
 import com.fangcang.titanjr.pay.rsp.TianConfirmBussOrderRsp;
 import com.fangcang.titanjr.pay.task.TitanPayResultNotifyTask;
 import com.fangcang.titanjr.pay.util.JsonConversionTool;
 import com.fangcang.titanjr.pay.util.TitanThreadPool;
+import com.fangcang.titanjr.request.AccountInfoRequest;
+import com.fangcang.titanjr.request.CheckPermissionRequest;
+import com.fangcang.titanjr.response.CheckAccountResponse;
+import com.fangcang.titanjr.response.PermissionResponse;
 import com.fangcang.titanjr.service.TitanFinancialTradeService;
+import com.fangcang.util.StringUtil;
 
 /**
  * 收銀台交易服務實現類
@@ -36,6 +44,9 @@ public class FinancialTradeService {
 
 	@Resource
 	private TitanFinancialTradeService titanFinancialTradeService;
+
+	@Resource
+	private TitanFinancialPermissionFacade titanFinancialPermissionFacade;
 
 	/**
 	 * 确认业务的订单信息
@@ -68,20 +79,85 @@ public class FinancialTradeService {
 		return null;
 	}
 
-	// /**
-	// * 验证订单是否已经产生交易或者是还处于处理中状态
-	// * @param req
-	// * @return
-	// */
-	// public TitanCreateOrderRsp createTitanOrder(
-	// TitanOrderRequest dto) {
-	//
-	//
-	//
-	//
-	//
-	// return null;
-	// }
+	/**
+	 * 检查付款和付款方的权限
+	 * 
+	 * @param dto
+	 * @return
+	 */
+	public boolean checkPermission(TitanOrderRequest dto) {
+		PayerTypeEnum pe = PayerTypeEnum.getPayerTypeEnumByKey(dto
+				.getPayerType());
+
+		CheckPermissionRequest permissionRequest = null;
+		PermissionResponse permissionResponse = null;
+
+		if (pe.isFcUserId() && StringUtil.isValidString(dto.getUserId())) {
+			permissionRequest = new CheckPermissionRequest();
+			permissionRequest.setFcuserid(dto.getUserId());
+			permissionRequest.setPermission("1");
+
+			log.info("check permission userId= " + dto.getUserId());
+
+			permissionResponse = titanFinancialPermissionFacade
+					.isPermissionToPayment(permissionRequest);
+
+			if (permissionResponse == null || (!permissionResponse.isResult())) {
+				if (permissionResponse != null) {
+					log.error("checkPermission response  "
+							+ permissionResponse.getReturnCode() + ":"
+							+ permissionResponse.getReturnMessage());
+				}
+				return false;
+			}
+		}
+
+		if (pe.isReicveMerchantCode()
+				&& StringUtil.isValidString(dto.getRuserId())) {
+			AccountInfoRequest accountInfo = new AccountInfoRequest();
+			accountInfo.setMerchantCode(dto.getRuserId());
+			log.info("check permission ruserId= " + dto.getRuserId());
+
+			CheckAccountResponse response = titanFinancialPermissionFacade
+					.isFinanceAccount(accountInfo);
+
+			if (response == null || (!response.isResult())) {
+				if (response != null) {
+					log.error("checkPermission response  "
+							+ response.getReturnCode() + ":"
+							+ response.getReturnMessage());
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 根据指定的URL确认业务订单的金额
+	 * 
+	 * @param dto
+	 * @return
+	 */
+	public boolean checkConfirmBussOrder(TitanOrderRequest dto) {
+		if (StringUtil.isValidString(dto.getCheckOrderUrl())) {
+			TitanConfirmBussOrderReq req = new TitanConfirmBussOrderReq();
+			req.setAmount(dto.getAmount());
+			req.setBussOrderNo(dto.getGoodsId());
+			req.setUrl(dto.getCheckOrderUrl());
+			TianConfirmBussOrderRsp bussOrderRsp = this.confirmBussOrder(req);
+
+			if (bussOrderRsp == null || !bussOrderRsp.isSuccess()) {
+				if (bussOrderRsp != null) {
+					log.error("checkConfirmBussOrder response  "
+							+ bussOrderRsp.getResult().getResMsg());
+				}
+				return false;
+			}
+
+		}
+		return true;
+	}
 
 	/**
 	 * 通知业务付款结果
