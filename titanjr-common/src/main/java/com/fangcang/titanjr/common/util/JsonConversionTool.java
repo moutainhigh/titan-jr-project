2d28e723fda3b206c163cee265ecc9adb3770ab0
wengxitao <xitao.weng@fangcang.com>
2016-08-31 11:31:29 +0800

    -

---
+++ titanjr-common/src/main/java/com/fangcang/titanjr/common/util/JsonConversionTool.java
@@ -0,0 +1,30 @@
+package com.fangcang.titanjr.common.util;
+
+import com.google.gson.Gson;
+
+/**
+ * json转换工具
+ * 
+ * @author wengxitao
+ *
+ */
+public final class JsonConversionTool {
+	private final static Gson gson = new Gson();
+
+	public static <T> T toObject(String json, Class t) {
+		if (json == null) {
+			return null;
+		}
+
+		return (T) gson.fromJson(json, t);
+	}
+
+	public static String toJson(Object object) {
+		if (object == null) {
+			return null;
+		}
+
+		return gson.toJson(object);
+	}
+
+}
