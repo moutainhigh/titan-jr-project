83af204d91da55f4139ad1227795d49378e4673e
fangdaikang <fangdaikang@163.com>
2016-09-11 14:47:29 +0800

    ID:修改master的回调地址

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialTradeServiceImpl.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialTradeServiceImpl.java
@@ -2428,8 +2428,8 @@ public class TitanFinancialTradeServiceImpl implements TitanFinancialTradeServic
 			String domainName = domainConfigDao.queryCurrentEnvDomain();
 			if(StringUtil.isValidString(domainName)){
 				payMethodConfigDTO = new PayMethodConfigDTO();
-				payMethodConfigDTO.setPageurl("http://"+domainName+"/titanjr-pay-app/payment/payConfirmPage.action");
-				payMethodConfigDTO.setNotifyurl("http://"+domainName+"/titanjr-pay-app/payment/notify.action");
+				payMethodConfigDTO.setPageurl("http://"+domainName+"/TFS/payment/payConfirmPage.action");
+				payMethodConfigDTO.setNotifyurl("http://"+domainName+"/TFS/payment/notify.action");
 			}
 			return payMethodConfigDTO;
 			
