b63682025bb004457759795e3320e7447c5ca7eb
luoqinglong <qinglong.luo@fangcang.com>
2016-10-17 09:43:44 +0800

    解决冲突

--- titanjr-rs-app/src/main/java/com/fangcang/titanjr/rs/manager/impl/ROPClient.java
+++ titanjr-rs-app/src/main/java/com/fangcang/titanjr/rs/manager/impl/ROPClient.java
@@ -1,5 +1,17 @@
 package com.fangcang.titanjr.rs.manager.impl;
 
-public class ROPClient {
+import com.Rop.api.ApiException;
+import com.Rop.api.DefaultRopClient;
+import com.Rop.api.RopRequest;
+import com.Rop.api.RopResponse;
 
+public class ROPClient {
+	//融数调用构造的client
+    public DefaultRopClient ropClient = null;
+    public String sessionKey = null;
+    
+    public  <T extends RopRequest<RopResponse>>  RopResponse execute(T r) throws ApiException{
+    	return ropClient.execute(r, sessionKey);
+    	
+    }
 }
