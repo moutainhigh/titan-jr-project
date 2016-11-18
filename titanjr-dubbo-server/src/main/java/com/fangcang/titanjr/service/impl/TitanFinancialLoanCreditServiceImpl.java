a287680da3bb8d95a63ef222e874a2a6ec219d84
zhaoshan <15572760238@163.com>
2016-11-18 14:37:48 +0800

    暂存

---
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialLoanCreditServiceImpl.java
@@ -0,0 +1,36 @@
+package com.fangcang.titanjr.service.impl;
+
+import com.fangcang.titanjr.dto.request.ApplyLoanCreditRequest;
+import com.fangcang.titanjr.dto.request.AuditCreidtOrderRequest;
+import com.fangcang.titanjr.dto.request.GetCreditInfoRequest;
+import com.fangcang.titanjr.dto.request.LoanCreditSaveRequest;
+import com.fangcang.titanjr.dto.response.ApplyLoanCreditResponse;
+import com.fangcang.titanjr.dto.response.AuditCreidtOrderResponse;
+import com.fangcang.titanjr.dto.response.GetCreditInfoResponse;
+import com.fangcang.titanjr.dto.response.LoanCreditSaveResponse;
+import com.fangcang.titanjr.service.TitanFinancialLoanCreditService;
+
+/**
+ * Created by zhaoshan on 2016/11/18.
+ */
+public class TitanFinancialLoanCreditServiceImpl implements TitanFinancialLoanCreditService {
+    @Override
+    public AuditCreidtOrderResponse auditCreditOrder(AuditCreidtOrderRequest req) {
+        return null;
+    }
+
+    @Override
+    public GetCreditInfoResponse getCreditOrderInfo(GetCreditInfoRequest req) {
+        return null;
+    }
+
+    @Override
+    public LoanCreditSaveResponse saveCreditOrder(LoanCreditSaveRequest req) {
+        return null;
+    }
+
+    @Override
+    public ApplyLoanCreditResponse applyLoanCredit(ApplyLoanCreditRequest req) {
+        return null;
+    }
+}
