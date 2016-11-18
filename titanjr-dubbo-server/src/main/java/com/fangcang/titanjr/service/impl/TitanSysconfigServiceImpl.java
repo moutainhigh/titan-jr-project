92ed04b3865cd1eb5a948f895c8e214fadde2007
luoqinglong <qinglong.luo@fangcang.com>
2016-11-18 16:11:24 +0800

    增加获取ftp接口

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanSysconfigServiceImpl.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanSysconfigServiceImpl.java
@@ -1,43 +1,20 @@
 package com.fangcang.titanjr.service.impl;
 
 
-import javax.annotation.Resource;
-
-
-import com.fangcang.titanjr.dao.TitanSysconfigDao;
-import com.fangcang.titanjr.dto.request.RSInvokeConfigRequest;
-import com.fangcang.titanjr.dto.request.TitanCallBackConfigRequest;
-import com.fangcang.titanjr.dto.request.TitanPayMethodRequest;
-import com.fangcang.titanjr.dto.response.RSInvokeConfigResponse;
-import com.fangcang.titanjr.dto.response.TitanCallBackConfigResponse;
-import com.fangcang.titanjr.dto.response.TitanPayMethodResponse;
+import com.fangcang.titanjr.common.util.DubboServerJDBCProperties;
+import com.fangcang.titanjr.dto.response.FTPConfigResponse;
 import com.fangcang.titanjr.service.TitanSysconfigService;
 
 public class TitanSysconfigServiceImpl implements TitanSysconfigService {
-	@Resource
-	private TitanSysconfigDao titanSysconfigDao;
 	
-	/**
-	 * 请参照RSInvokeInitManagerImpl.java，从中获取
-	 */
 	@Override
-	public RSInvokeConfigResponse getRSInvokeConfig(RSInvokeConfigRequest request) {
-		
-		return null;
-	}
-
-	@Override
-	public TitanPayMethodResponse getTitanPayMethod(
-			TitanPayMethodRequest request) {
-		
-		return null;
-	}
-
-	@Override
-	public TitanCallBackConfigResponse getTitanCallBackConfig(
-			TitanCallBackConfigRequest request) {
-		 
-		return null;
+	public FTPConfigResponse getFTPConfig() {
+		FTPConfigResponse response = new FTPConfigResponse();
+		response.setFtpServerIp(DubboServerJDBCProperties.getFtpServerIp());
+		response.setFtpServerPort(DubboServerJDBCProperties.getFtpServerPort());
+		response.setFtpServerUser(DubboServerJDBCProperties.getFtpServerUser());
+		response.setFtpServerPassword(DubboServerJDBCProperties.getFtpServerPassword());		
+		return response;
 	}
 	
 }
