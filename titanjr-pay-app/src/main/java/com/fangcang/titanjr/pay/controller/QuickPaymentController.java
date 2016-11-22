008fc9428fcd21e04ab5724f1809a47d38cde606
fangdaikang <fangdaikang@163.com>
2016-11-22 14:09:40 +0800

    ID:

--- titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/QuickPaymentController.java
+++ titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/QuickPaymentController.java
@@ -1,24 +1,14 @@
 package com.fangcang.titanjr.pay.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpRequest;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.Rop.api.internal.stream.connect.HttpResponse;
-import com.fangcang.titanjr.common.enums.PayerTypeEnum;
-import com.fangcang.titanjr.dto.bean.OrgBindInfo;
-import com.fangcang.titanjr.dto.bean.TransOrderDTO;
-import com.fangcang.titanjr.dto.request.RechargeResultConfirmRequest;
 
 @RequestMapping("/quickPayment")
 @Controller
@@ -28,8 +18,6 @@ public class QuickPaymentController extends BaseController{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private static final Log log = LogFactory.getLog(QuickPaymentController.class);
 	
 	
 	@RequestMapping("customerNotify")
