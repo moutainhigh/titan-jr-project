ad099f1fb0155877dd0dd9265c73e6094c468043
fangdaikang <fangdaikang@163.com>
2016-11-30 18:05:02 +0800

    ID:冻结

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialLoanServiceImpl.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialLoanServiceImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.transaction.annotation.Isolation;
@@ -14,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fangcang.titanjr.common.enums.LoanApplyOrderEnum;
 import com.fangcang.titanjr.common.enums.LoanProductEnum;
 import com.fangcang.titanjr.common.util.CommonConstant;
+import com.fangcang.titanjr.common.util.DateUtil;
+import com.fangcang.titanjr.common.util.OrderGenerateService;
 import com.fangcang.titanjr.dao.LoanCreditOrderDao;
 import com.fangcang.titanjr.dao.LoanOrderDao;
 import com.fangcang.titanjr.dao.LoanRoomPackSpecDao;
@@ -39,6 +44,10 @@ import com.fangcang.titanjr.dto.response.SaveLoanOrderInfoResponse;
 import com.fangcang.titanjr.entity.LoanApplyOrder;
 import com.fangcang.titanjr.entity.LoanCreditOrder;
 import com.fangcang.titanjr.entity.LoanRoomPackSpec;
+import com.fangcang.titanjr.rs.dto.NewLoanApplyJsonData;
+import com.fangcang.titanjr.rs.manager.RSCreditManager;
+import com.fangcang.titanjr.rs.request.NewLoanApplyRequest;
+import com.fangcang.titanjr.rs.response.NewLoanApplyResponse;
 import com.fangcang.titanjr.service.TitanFinancialLoanService;
 import com.fangcang.util.StringUtil;
 
@@ -47,14 +56,17 @@ public class TitanFinancialLoanServiceImpl implements TitanFinancialLoanService{
 	private static final Log log = LogFactory
 			.getLog(TitanFinancialLoanServiceImpl.class);
 	
-//	@Resource 
+	@Resource 
 	private LoanOrderDao loanOrderDao;
 	
-//	@Resource
+	@Resource
 	private LoanRoomPackSpecDao loanRoomPackSpecDao;
 	
-//	@Resource
+	@Resource
 	private LoanCreditOrderDao loanCreditOrderDao;
+	
+	@Resource 
+	private RSCreditManager rsCreditManager;
 
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = Exception.class)
@@ -79,19 +91,31 @@ public class TitanFinancialLoanServiceImpl implements TitanFinancialLoanService{
 				
 			}
 			
+			//保存贷款申请单
 			boolean flag = saveLoanOrderBean(req.getLcanSpec(), productType.getCode(), req.getOrgCode());
 			if(!flag){
 				throw new Exception("保存订单失败");
 			}
-			//申请贷款
 			
+			//上传文件到融数，并且获取相应的urlKey
+			String urlKey = "150a15fa-b2fa-4c72-95b5-20f173c47814";
+			
+			//申请贷款
+			NewLoanApplyRequest request = this.convertToNewLoanApplyRequest(req.getLcanSpec(), productType.getCode(), req.getOrgCode());
+			request.setUrlkey(urlKey);
+			this.packLoanJSonData(request, req.getLcanSpec(), productType.getCode());
+			NewLoanApplyResponse loanResponse = rsCreditManager.newLoanApply(request);
+			if(!loanResponse.isSuccess()){
+				log.error(loanResponse.getReturnCode()+":"+loanResponse.getReturnMsg());
+				throw new Exception(loanResponse.getReturnCode()+":"+loanResponse.getReturnMsg());
+			}
+			response.putSuccess();
+			return response;
 			
 		}catch(Exception e){
 			log.error("贷款申请异常",e);
 			throw e;
 		}
-		
-		return null;
 	}
 	
 	private boolean saveLoanRoomPackSpecBean(LoanRoomPackSpecBean loanSpecBean) throws Exception{
@@ -120,7 +144,7 @@ public class TitanFinancialLoanServiceImpl implements TitanFinancialLoanService{
 		LoanApplyOrder loanApplyOrder = new LoanApplyOrder();
 		try{
 			
-			LoanRoomPackSpecBean loanRoomPackSpecBean  = (LoanRoomPackSpecBean)loanSpecBean;
+			LoanRoomPackSpecBean loanRoomPackSpecBean  = null;
 			
 			LoanCreditOrder loanCreditOrder = new LoanCreditOrder();
 			loanCreditOrder.setOrgCode(orgCode);
@@ -130,6 +154,7 @@ public class TitanFinancialLoanServiceImpl implements TitanFinancialLoanService{
 			}
 			loanCreditOrder =  loanCreditOrderList.get(0);
 			if(LoanProductEnum.ROOM_PACK.getCode()==type.intValue()){
+				loanRoomPackSpecBean = (LoanRoomPackSpecBean)loanSpecBean;
 				loanApplyOrder.setCreditOrderNo(loanApplyOrder.getCreditOrderNo());
 				loanApplyOrder.setOrderNo(loanRoomPackSpecBean.getLoanOrderNo());
 				if(StringUtil.isValidString(loanRoomPackSpecBean.getAmount())){
@@ -159,12 +184,61 @@ public class TitanFinancialLoanServiceImpl implements TitanFinancialLoanService{
 		}
 	}
 	
-	private boolean addLoanOrder(LoanOrderBean loanOrderBean){
 	
-		return false;
+	private NewLoanApplyRequest convertToNewLoanApplyRequest(LoanSpecBean loanSpecBean,Integer type,String orgCode) throws Exception{
+		try{
+			NewLoanApplyRequest request = new NewLoanApplyRequest();
+			LoanRoomPackSpecBean loanRoomPackSpecBean  = null;
+			if(LoanProductEnum.ROOM_PACK.getCode()==type.intValue()){
+				loanRoomPackSpecBean = (LoanRoomPackSpecBean)loanSpecBean;
+				request.setAmount(loanRoomPackSpecBean.getAmount());
+				request.setRootinstcd(CommonConstant.RS_FANGCANG_CONST_ID);
+				request.setProductid(LoanApplyOrderEnum.ProductId.LOAN_PRODUCTID.productId);
+				request.setRatetemplate(CommonConstant.RATE_TEMPLETE);
+				request.setReqesttime(DateUtil.sdf4.format(new Date()));
+				request.setRequestdate(DateUtil.sdf4.format(new Date()));
+				request.setUserid(orgCode);
+				request.setUserorderid(OrderGenerateService.genSyncUserOrderId());
+				//暂时还未确定TODO
+				request.setUserrelateid("TJM10000110");
+				request.setUsername("罗庆龙");
+			}else if(LoanProductEnum.OPERACTION.getCode()==type.intValue()){
+				
+			}
+			return request;
+		}catch(Exception e){
+			log.error("申请贷款失败");
+			throw e;
+		}
+	}
+
+	private void packLoanJSonData(NewLoanApplyRequest request,LoanSpecBean loanSpecBean,Integer type){
+		NewLoanApplyJsonData moneyCreditJsonData = new NewLoanApplyJsonData();
+		LoanRoomPackSpecBean loanRoomPackSpecBean  = null;
+		if(LoanProductEnum.ROOM_PACK.getCode()==type.intValue()){
+			loanRoomPackSpecBean = (LoanRoomPackSpecBean)loanSpecBean;
+		}
+    	moneyCreditJsonData.setLoanApplicateName(loanRoomPackSpecBean.getAccountName());
+    	moneyCreditJsonData.setInParty(request.getUsername());
+    	moneyCreditJsonData.setUserOrderId(request.getUserorderid());
+    	moneyCreditJsonData.setOrderTime(request.getReqesttime());
+    	moneyCreditJsonData.setProductName(loanRoomPackSpecBean.getHotleName());
+    	moneyCreditJsonData.setInBankAccount(loanRoomPackSpecBean.getBank());
+    	moneyCreditJsonData.setInBankAccountNo(loanRoomPackSpecBean.getAccount());
+    	moneyCreditJsonData.setDeliveryStatus("");
+    	moneyCreditJsonData.setReceivingState("");
+    	moneyCreditJsonData.setReceiptAddress("深圳市宝安区");
+    	moneyCreditJsonData.setCode("10000000000009");
+    	moneyCreditJsonData.setUnitPrice("");
+    	moneyCreditJsonData.setNumber(loanRoomPackSpecBean.getRoomNights()+"");
+    	moneyCreditJsonData.setOrderAmount(loanRoomPackSpecBean.getAmount());
+    	moneyCreditJsonData.setOrderType("");
+    	moneyCreditJsonData.setRootInstCd(request.getRootinstcd());
+    	moneyCreditJsonData.setLoanTerm("60");
+    	JSON result = JSONSerializer.toJSON(moneyCreditJsonData);
+    	request.setJsondata(result.toString());
 	}
 	
-
 	@Override
 	public CancelLoanResponse cancelLoan(CancelLoanRequest req) {
 		// TODO Auto-generated method stub
