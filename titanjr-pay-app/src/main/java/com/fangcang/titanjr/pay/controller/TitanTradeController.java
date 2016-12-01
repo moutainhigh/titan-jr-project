46857e946f95739cabd233c23c45dbe759b7b113
fangdaikang <fangdaikang@163.com>
2016-12-01 16:04:35 +0800

    ID:金额

--- titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/TitanTradeController.java
+++ titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/TitanTradeController.java
@@ -312,12 +312,10 @@ public class TitanTradeController extends BaseController {
           }
 		}
 		
-		if(new BigDecimal(dto.getAmount()).compareTo(BigDecimal.ZERO)<1){
+		if(pe.isOpenOrg() &&  new BigDecimal(dto.getAmount()).compareTo(BigDecimal.ZERO)<1){
 			log.error("Amount not regular ");
 			return false;
 		}
-		
-		
 		
 		if(StringUtil.isValidString(dto.getCurrencyType()) && !dto.getCurrencyType().equals(CommonConstant.CURRENT_TYPE)){
 			log.error("Currency type must be RMB ");
