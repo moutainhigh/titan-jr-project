69e202c5bf8a10b821639b7d5249aaeba5402b2e
wengxitao <xitao.weng@fangcang.com>
2016-12-22 17:22:42 +0800

    ---

---
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/entity/PlatformOrderStat.java
@@ -0,0 +1,84 @@
+package com.fangcang.titanjr.entity;
+
+public class PlatformOrderStat {
+
+	private String roomNight;
+	
+	private String paidin;
+	
+	private String exchangePaidin;
+	
+	private String receivable;
+	
+	private String exchangeReceivable;
+	
+	private String unReceivable;
+	
+	private String payable;
+	
+	private String profit;
+
+	public String getRoomNight() {
+		return roomNight;
+	}
+
+	public void setRoomNight(String roomNight) {
+		this.roomNight = roomNight;
+	}
+
+	public String getPaidin() {
+		return paidin;
+	}
+
+	public void setPaidin(String paidin) {
+		this.paidin = paidin;
+	}
+
+	public String getExchangePaidin() {
+		return exchangePaidin;
+	}
+
+	public void setExchangePaidin(String exchangePaidin) {
+		this.exchangePaidin = exchangePaidin;
+	}
+
+	public String getReceivable() {
+		return receivable;
+	}
+
+	public void setReceivable(String receivable) {
+		this.receivable = receivable;
+	}
+
+	public String getExchangeReceivable() {
+		return exchangeReceivable;
+	}
+
+	public void setExchangeReceivable(String exchangeReceivable) {
+		this.exchangeReceivable = exchangeReceivable;
+	}
+
+	public String getUnReceivable() {
+		return unReceivable;
+	}
+
+	public void setUnReceivable(String unReceivable) {
+		this.unReceivable = unReceivable;
+	}
+
+	public String getPayable() {
+		return payable;
+	}
+
+	public void setPayable(String payable) {
+		this.payable = payable;
+	}
+
+	public String getProfit() {
+		return profit;
+	}
+
+	public void setProfit(String profit) {
+		this.profit = profit;
+	}
+}
