0d390b1f724212eb815fa0489933b28ecbed6253
wengxitao <xitao.weng@fangcang.com>
2017-04-27 14:10:36 +0800

    增加支行信息选择

--- titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/TitanAccountController.java
+++ titanjr-pay-app/src/main/java/com/fangcang/titanjr/pay/controller/TitanAccountController.java
@@ -1,5 +1,12 @@
 package com.fangcang.titanjr.pay.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,8 +20,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fangcang.titanjr.common.enums.TitanMsgCodeEnum;
 import com.fangcang.titanjr.common.exception.GlobalServiceException;
+import com.fangcang.titanjr.common.util.CommonConstant;
 import com.fangcang.titanjr.common.util.JsonConversionTool;
 import com.fangcang.titanjr.dto.bean.AccountHistoryDTO;
+import com.fangcang.titanjr.dto.bean.CityInfoDTO;
 import com.fangcang.titanjr.dto.bean.TitanUserBindInfoDTO;
 import com.fangcang.titanjr.dto.request.AccountCheckRequest;
 import com.fangcang.titanjr.dto.request.AccountHistoryRequest;
@@ -23,6 +32,7 @@ import com.fangcang.titanjr.dto.request.PayPasswordRequest;
 import com.fangcang.titanjr.dto.response.AccountCheckResponse;
 import com.fangcang.titanjr.dto.response.AccountHistoryResponse;
 import com.fangcang.titanjr.dto.response.BankInfoResponse;
+import com.fangcang.titanjr.dto.response.CityInfosResponse;
 import com.fangcang.titanjr.dto.response.PayPasswordResponse;
 import com.fangcang.titanjr.pay.services.TitanPaymentService;
 import com.fangcang.titanjr.service.TitanFinancialAccountService;
@@ -125,8 +135,8 @@ public class TitanAccountController extends BaseController {
 
 	@ResponseBody
 	@RequestMapping("/check_payPassword")
-	public String checkPayPassword(String payPassword, String fcUserid,String tfsUserid)
-			throws GlobalServiceException {
+	public String checkPayPassword(String payPassword, String fcUserid,
+			String tfsUserid) throws GlobalServiceException {
 		String ttfsUserid = null;
 		if (StringUtil.isValidString(fcUserid)) {
 			TitanUserBindInfoDTO titanUserBindInfoDTO = new TitanUserBindInfoDTO();
@@ -137,10 +147,10 @@ public class TitanAccountController extends BaseController {
 					&& titanUserBindInfoDTO.getTfsuserid() != null) {
 				ttfsUserid = titanUserBindInfoDTO.getTfsuserid().toString();
 			}
-		}else{
+		} else {
 			ttfsUserid = tfsUserid;
 		}
-		
+
 		if (!StringUtil.isValidString(payPassword)
 				|| !StringUtil.isValidString(ttfsUserid)) {
 			return toMsgJson(TitanMsgCodeEnum.PARAMETER_VALIDATION_FAILED);
@@ -222,14 +232,101 @@ public class TitanAccountController extends BaseController {
 		return "checkstand-pay/putPayPassword";
 	}
 
+	private Map<String, CityInfoDTO> getParentCity() {
+		CityInfoDTO cityInfo = new CityInfoDTO();
+		cityInfo.setDataType(1);
+		CityInfosResponse response = titanFinancialAccountService
+				.getCityInfoList(cityInfo);
+
+		Map<String, CityInfoDTO> citys = new HashMap<String, CityInfoDTO>();
+		for (CityInfoDTO city : response.getCityInfoDTOList()) {// 将其code和name放到键值对中
+			citys.put(city.getCityCode(), city);
+		}
+
+		Set<String> cityCodes = new HashSet<String>(citys.keySet());
+		for (String cityCode : cityCodes) {
+			cityInfo.setDataType(null);
+			cityInfo.setParentCode(cityCode);
+			response = titanFinancialAccountService.getCityInfoList(cityInfo);
+			for (CityInfoDTO city : response.getCityInfoDTOList()) {// 将其code和name放到键值对中
+				citys.put(city.getCityCode(), city);
+			}
+		}
+		return citys;
+	}
+
+	private List<CityInfoDTO> getCity() {
+		List<CityInfoDTO> citys = new ArrayList<CityInfoDTO>();
+
+		CityInfoDTO cityInfo = new CityInfoDTO();
+		cityInfo.setDataType(1);
+		CityInfosResponse response = titanFinancialAccountService
+				.getCityInfoList(cityInfo);
+
+		// for(CityInfoDTO city : response.getCityInfoDTOList()
+		// ){//将其code和name放到键值对中
+		// citys.add(city);
+		// }
+
+		for (CityInfoDTO city : response.getCityInfoDTOList()) {
+			cityInfo.setDataType(null);
+			cityInfo.setParentCode(city.getCityCode());
+			response = titanFinancialAccountService.getCityInfoList(cityInfo);
+			for (CityInfoDTO cityinfo : response.getCityInfoDTOList()) {// 将其code和name放到键值对中
+				citys.add(cityinfo);
+			}
+		}
+		return citys;
+	}
+
+	private String getCityName(CityInfoDTO city,
+			Map<String, CityInfoDTO> cityMap) {
+		if (city == null || !StringUtil.isValidString(city.getCityCode())) {
+			return "";
+		}
+		if (CommonConstant.BEIJING_CODE.equals(city.getCityCode())
+				|| CommonConstant.TIANJING_CODE.equals(city.getCityCode())
+				|| CommonConstant.CHONGQING_CODE.equals(city.getCityCode())
+				|| CommonConstant.SHNAGHAI_CODE.equals(city.getCityCode())) {// 直辖市
+			return city.getCityName();
+		}
+		StringBuffer cityName = new StringBuffer(city.getCityName());
+		city = cityMap.get(city.getParentCode());
+		if (city == null) {
+			return cityName.toString();
+		}
+		cityName = cityName.insert(0, "-").insert(0, city.getCityName());
+		city = cityMap.get(city.getParentCode());
+		if (city == null) {
+			return cityName.toString();
+		}
+		return cityName.insert(0, "-").insert(0, city.getCityName()).toString();
+	}
+
+	@ResponseBody
+	@RequestMapping("getCitys")
+	public String getCitys() {
+		CityInfosResponse response = new CityInfosResponse();
+		Map<String, CityInfoDTO> cityMap = this.getParentCity();
+		List<CityInfoDTO> cityInfos = getCity();
+		for (CityInfoDTO city : cityInfos) {
+			city.setCityName(this.getCityName(city, cityMap));
+		}
+		response.setCityInfoDTOList(cityInfos);
+		return JsonConversionTool.toJson(response);
+	}
+
 	@ResponseBody
 	@RequestMapping("getBankInfoList")
-	 public String getBankInfo(BankInfoQueryRequest request){
-	        BankInfoResponse bankInfoResponse =  titanFinancialBaseInfoService.queryBankInfoList(request);
-	        if (bankInfoResponse.isResult() && CollectionUtils.isNotEmpty(bankInfoResponse.getBankInfoDTOList())){
-	        	return JsonConversionTool.toJson(bankInfoResponse);
-	        }
-	        return null;
-	    }
-	
+	public String getBankInfo(BankInfoQueryRequest request) {
+		BankInfoResponse bankInfoResponse = titanFinancialBaseInfoService
+				.queryBankInfoList(request);
+		if (bankInfoResponse.isResult()
+				&& CollectionUtils.isNotEmpty(bankInfoResponse
+						.getBankInfoDTOList())) {
+			return JsonConversionTool.toJson(bankInfoResponse);
+		}
+		return null;
+	}
+
 }
