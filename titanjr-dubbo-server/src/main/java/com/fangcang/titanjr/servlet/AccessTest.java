be6cdf6f4d8f4351a7dcb274e8f3572e4eed8a39
Administrator <Administrator@local.fangcang.com>
2017-06-09 14:14:47 +0800

    转载失败，重复发邮件问题修复

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/servlet/AccessTest.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/servlet/AccessTest.java
@@ -2,13 +2,12 @@ package com.fangcang.titanjr.servlet;
 
 import com.fangcang.titanjr.dao.TitanSysConfigDao;
 import com.fangcang.util.SpringContextUtil;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 /**
@@ -16,7 +15,12 @@ import java.io.IOException;
  */
 public class AccessTest extends HttpServlet {
 
-    @Override
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -652780215186592866L;
+
+	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TitanSysConfigDao titanSysConfigDao = (TitanSysConfigDao) SpringContextUtil.getBean("titanSysConfigDao");
         titanSysConfigDao.accessQueryTest();
