5f75cb714672d076a070c6b9b2bf52aa81c0f452
pengjiawei <jiawei.peng@fangcang.com>
2017-06-13 14:17:00 +0800

    代码优化

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialTradeServiceImpl.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/service/impl/TitanFinancialTradeServiceImpl.java
@@ -10,8 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
 import javax.annotation.Resource;
+
 import net.sf.json.JSONSerializer;
+
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
