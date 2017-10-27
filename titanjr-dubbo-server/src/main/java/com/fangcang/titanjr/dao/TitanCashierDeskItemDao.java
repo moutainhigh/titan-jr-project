d6788c5fb03642ecba80d2795414563410f48e27
pengjiawei <jiawei.peng@fangcang.com>
2017-10-27 09:43:05 +0800

    初始化交易平台收银台

--- titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/dao/TitanCashierDeskItemDao.java
+++ titanjr-dubbo-server/src/main/java/com/fangcang/titanjr/dao/TitanCashierDeskItemDao.java
@@ -3,6 +3,7 @@ package com.fangcang.titanjr.dao;
 import java.util.List;
 
 import com.fangcang.exception.DaoException;
+import com.fangcang.titanjr.dto.bean.CashierDeskDTO;
 import com.fangcang.titanjr.entity.TitanCashierDeskItem;
 
 
@@ -13,4 +14,18 @@ public interface TitanCashierDeskItemDao {
 	int updateCashierDeskItem(TitanCashierDeskItem entity) throws DaoException;
 	
 	List<TitanCashierDeskItem> queryCashierDeskItems(TitanCashierDeskItem entity);
+	
+	/**
+	 * 删除所有交易平台收银台的支付方式
+	 * @author Jerry
+	 * @date 2017年10月26日 下午5:27:40
+	 */
+	public int delCashierdeskItemForTradingPlatform();
+	
+	/**
+	 * 查询交易平台收银台deskid
+	 * @author Jerry
+	 * @date 2017年10月26日 下午2:33:46
+	 */
+	public List<CashierDeskDTO> queryTradingPlatformCashierdesk();
 }
