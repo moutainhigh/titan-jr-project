fbfdb71e6657dda39bc6e4f72403b169d90f2bbc
luoqinglong <qinglong.luo@fangcang.com>
2017-11-08 16:22:14 +0800

    修改功能描述

--- titanjr-facade-server/src/main/java/com/fangcang/titanjr/web/config/TitanFinancialSwaggerConfig.java
+++ titanjr-facade-server/src/main/java/com/fangcang/titanjr/web/config/TitanFinancialSwaggerConfig.java
@@ -35,7 +35,7 @@ public class TitanFinancialSwaggerConfig {
                 "提供金融服务化接口，包括机构注册，用户管理，账户基础管理，提现退款能功能",
                 "接口服务条款信息在这里",
                 "请联系泰坦金融开发团队团队jinrong@fangcang.com",
-                "接口访问许可类型：至允许内部访问",
+                "接口访问许可类型：只允许内部访问",
                 "应用程序接口访问许可文件地址");
         return apiInfo;
     }
