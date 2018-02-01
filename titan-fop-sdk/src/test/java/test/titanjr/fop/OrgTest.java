27a17ce1e321dc6f0e266e6a909279b33aed6f78
luoqinglong <qinglong.luo@fangcang.com>
2018-02-01 18:10:29 +0800

    测试企业机构更新

--- titan-fop-sdk/src/test/java/test/titanjr/fop/OrgTest.java
+++ titan-fop-sdk/src/test/java/test/titanjr/fop/OrgTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 
 import com.fangcang.titanjr.common.util.Tools;
 import com.titanjr.fop.exceptions.ApiException;
+import com.titanjr.fop.request.WheatfieldEnterpriseUpdatecompanyinfoRequest;
 import com.titanjr.fop.request.WheatfieldPersonAccountoprRequest;
+import com.titanjr.fop.response.WheatfieldEnterpriseUpdatecompanyinfoResponse;
 import com.titanjr.fop.response.WheatfieldPersonAccountoprResponse;
 
 public class OrgTest extends BaseTest {
 	
 	
-	@Test
+	//@Test
 	public void updatePersonOrg(){
 		
 		WheatfieldPersonAccountoprRequest personAccountoprRequest = new WheatfieldPersonAccountoprRequest();
@@ -27,6 +29,27 @@ public class OrgTest extends BaseTest {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	@Test
+	public void updateEnterpriseOrg(){
+		WheatfieldEnterpriseUpdatecompanyinfoRequest enterpriseUpdateRequest = new WheatfieldEnterpriseUpdatecompanyinfoRequest();
+		
+		enterpriseUpdateRequest.setOpertype("1");
+		enterpriseUpdateRequest.setUserid("TJM10000090");
+		enterpriseUpdateRequest.setConnect("connect1111");
+		enterpriseUpdateRequest.setUsername("username11111");
+		enterpriseUpdateRequest.setCorporatename("luoqinglongname");
+		enterpriseUpdateRequest.setBuslince("1111");
+		
+		
+		try {
+			WheatfieldEnterpriseUpdatecompanyinfoResponse response = fopClient.execute(enterpriseUpdateRequest);
+			System.out.println(Tools.gsonToString(response));
+		
+		} catch (ApiException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
